985b066a47d250586c512a2cb6f15bfb2f54a71a
Dynamic-Form-Generator <gurampatisrinivasulurreddy@gmail.com>
2024-11-25 14:49:33 +0530

    done

---
+++ src/components/Layout.tsx
@@ -0,0 +1,12 @@
+// Layout.tsx
+import React from 'react'; // Importing React makes the file a module
+export {};
+const Layout = () => {
+  return (
+    <div>
+      {/* Your layout content */}
+    </div>
+  );
+};
+
+export default Layout;
